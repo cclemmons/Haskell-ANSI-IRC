7fc69de3eb46e241d87dcf16e4274acfdba2ccfc
Colin Clemmons <clemmons@stanford.edu>
2016-03-12 21:51:10 -0800

    Fixes all type errors in Lib

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Lib
-    ( someFunc
+    ( mainApp
     ) where
 
 import Control.Exception
@@ -10,19 +10,21 @@ import Control.Concurrent.Chan
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TChan
 import Control.Monad
-import Data.Monoid
+import Data.Monoid ((<>))
 
 import Data.Time.Calendar
 import Data.Time.Clock
+import Data.List (sort)
 
 import System.IO
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Network.Socket
+import Network (listenOn, accept, PortID)
+import Network.Socket (Socket)
 
-import Data.ByteString (ByteString)
+import Data.ByteString (ByteString, pack)
 import qualified Data.ByteString as Bstr
 import Data.ByteString.Builder
 
@@ -33,16 +35,19 @@ data User = User { username :: ByteString
                  , userHndl :: Handle
                  , userActv :: ByteString
                  , userRooms :: Map ByteString (TChan Msg)}
-    deriving (Show, Eq)
+    deriving (Eq)
 
 data Msg = Msg { msgTime :: UTCTime
                , msgSender :: ByteString
                , msgRoom :: ByteString
                , msgCont :: ByteString}
-    deriving (Show, Eq, Ord)
+    deriving (Eq, Ord)
 
 utcToBuilder :: UTCTime -> Builder
-utcToBuilder t = stringUtf8 $ utctDay t
+utcToBuilder t = integerDec year <>. "-" <> intDec mon <>. "-" <> intDec day
+    where
+        (year, mon, day) = toGregorian $ utctDay t
+        a <>. b = a <> byteString b
 
 msgToBuilder :: Msg -> Builder
 msgToBuilder msg = byteString "<" <> username <>. " " <> room <> time <>. ">" <>." " <> cont <>. "\n"
@@ -57,34 +62,34 @@ sendMsgs :: User -> IO ()
 sendMsgs user = do
     let handle = userHndl user
     msgs <- atomically $ getMsgs user
-    foldl () ((>>) . hPutBuilder . msgToBuilder) msgs
+    foldl (\acc new -> acc >> (hPutBuilder handle $ msgToBuilder new)) (return ()) msgs
 
 getMsgs :: User -> STM [Msg]
-getMsgs user = liftM sort $ Map.foldr (concat) (return []) (tchs)
+getMsgs user = liftM sort $ Map.foldl (concat) (return []) (tchs)
     where concat acc tc = liftM2 (++) acc $ qdMsgs tc
           tchs = userRooms user
 
-qdMsgs :: TChan ByteString -> STM [Msg]
+qdMsgs :: TChan Msg -> STM [Msg]
 qdMsgs tc = do
     elem <- tryReadTChan tc
-    return $ case elem of
-                Nothing -> [] 
-                Just x -> (qdMsgs tc):x[]
+    case elem of
+        Nothing -> return []
+        Just x -> liftM2 (:) (return x) (qdMsgs tc)
 
 userHandler :: User -> IO ()
 userHandler user = do
     let handle = userHndl user
     ready <- hWaitForInput handle msgTimeOut
-    if ready then recvMsg user else ()
+    if ready then recvMsg user else return ()
     sendMsgs user
     userHandler user
 
 recvMsg :: User -> IO ()
 recvMsg user = do
-    bstr <- Bstr.hGetLine
+    bstr <- Bstr.hGetLine $ userHndl user
     t <- getCurrentTime
     let msg = Msg t (username user) (userActv user) bstr
-    atomically $ writeTchan ((userRooms user) Map.! (userActv user)) msg
+    atomically $ writeTChan ((userRooms user) Map.! (userActv user)) msg
 
 newUser :: Handle -> MVar (Map ByteString (TChan Msg))-> IO ()
 newUser hand rooms' = do
@@ -101,11 +106,11 @@ getUsername hand = do
     return name
 
 getUserRooms :: Handle -> IO (Set ByteString)
-getUserRooms = return $ Set.singleton ""
+getUserRooms hand = return $ Set.singleton ("" :: ByteString)
 
-acceptAndFork :: Socket -> MVar (Map ByteString (TChan Msg)) -> IO ()
+acceptAndFork :: Socket -> MVar (Map ByteString (TChan Msg)) -> IO ThreadId
 acceptAndFork sock rooms = do
-    (hand, _, _) <- NSock.accept sock
+    (hand, _, _) <- accept sock
     forkIO $ newUser hand rooms
 
 addRoom :: ByteString -> Map ByteString (TChan Msg) -> STM (Map ByteString (TChan Msg))
@@ -113,9 +118,9 @@ addRoom name map = do
     tc <- newTChan
     return $ Map.insertWith (flip const) name tc map
 
-mainapp :: String -> IO ()
-mainapp port = do
-    startingRooms <- (atomically $ addRoom "" Map.Empty)
-    let rooms = newMVar startingRooms 
+mainApp :: PortID -> IO ()
+mainApp port = do
+    startingRooms <- (atomically $ addRoom "" Map.empty)
+    rooms <- newMVar startingRooms 
     sock <- listenOn port
     forever $ acceptAndFork sock rooms
