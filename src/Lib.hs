e743af8eca0858c4c6839ac58d1bca2f438709e4
Colin Clemmons <clemmons@stanford.edu>
2016-03-15 21:02:14 -0700

    Adds bracketing to close handles and cleanup users, adds announcements for new users

--- src/Lib.hs
+++ src/Lib.hs
@@ -32,7 +32,7 @@ import Data.ByteString.Builder
 msgTimeOut :: Int
 msgTimeOut = 100
 
-data User = User { username :: ByteString
+data User = User { userName :: ByteString
                  , userHndl :: Handle
                  , userActv :: ByteString
                  , userRooms :: Map ByteString (TChan Msg)}
@@ -44,26 +44,28 @@ data Msg = Msg { msgTime :: UTCTime
                , msgCont :: ByteString}
     deriving (Eq, Ord)
 
---announce :: User -> IO ()
---announce user = do
---    let broadcast = systemMsg (username user ++ " has entered")
---    atomically $ Map.foldl (\acc room -> do acc >> broadcast room) (return ()) (userRooms user)
---    where
---        foldfn acc room = do
-            
+-- announces a user's entrance to all of their rooms
+announce :: User -> ByteString -> IO User
+announce user constr = do
+    let broadcast = systemMsg (Bstr.concat [userName user, constr])
+        foldfn acc roomname tchan = do
+            acc
+            msg <- broadcast roomname
+            atomically $ writeTChan tchan msg
+    Map.foldlWithKey foldfn (return ()) (userRooms user)
+    return user
 
+-- Generates a message from the system with the given room and text
 systemMsg :: ByteString -> ByteString -> IO Msg
 systemMsg str room = do
     t <- getCurrentTime
     return $ Msg t "System" room str
 
+-- Transforms a UTCTime into a Bytestring Builder
 utcToBuilder :: UTCTime -> Builder
 utcToBuilder t = stringUtf8 $ formatTime defaultTimeLocale "%R" t
-    --where
-    --    integerDec year <>. "-" <> intDec mon <>. "-" <> intDec day
-    --    (year, mon, day) = toGregorian $ utctDay t
-    --    a <>. b = a <> byteString b
 
+-- Formats a message into a bytestring builder
 msgToBuilder :: Msg -> Builder
 msgToBuilder msg = byteString "<" <> username <>. " " <> room <> time <>. ">" <>." " <> cont <>. "\n"
     where
@@ -73,24 +75,29 @@ msgToBuilder msg = byteString "<" <> username <>. " " <> room <> time <>. ">" <>
         time = utcToBuilder $ msgTime msg
         a <>. b = a <> byteString b
 
+-- Flushes all the messages from user's rooms to their handle
 sendMsgs :: User -> IO ()
 sendMsgs user = do
     let handle = userHndl user
     msgs <- atomically $ getMsgs user
     foldl (\acc new -> acc >> (hPutBuilder handle $ msgToBuilder new)) (return ()) msgs
 
+-- generates a list of Msgs from a user's rooms
 getMsgs :: User -> STM [Msg]
 getMsgs user = liftM sort $ Map.foldl (concat) (return []) (tchs)
-    where concat acc tc = liftM2 (++) acc $ qdMsgs tc
+    where concat acc tc = liftM2 (++) acc $ dQMsgs tc
           tchs = userRooms user
 
-qdMsgs :: TChan Msg -> STM [Msg]
-qdMsgs tc = do
+-- reads the messages from a given tchan into an list
+dQMsgs :: TChan Msg -> STM [Msg]
+dQMsgs tc = do
     elem <- tryReadTChan tc
     case elem of
         Nothing -> return []
-        Just x -> liftM2 (:) (return x) (qdMsgs tc)
+        Just x -> liftM2 (:) (return x) (dQMsgs tc)
 
+-- Final user thread function, checks for input and then recieves if
+-- necessary and sends information to the user
 userHandler :: User -> IO ()
 userHandler user = do
     let handle = userHndl user
@@ -99,13 +106,15 @@ userHandler user = do
     sendMsgs user
     userHandler user
 
+-- recieves a user's message and writes it to their active channel
 recvMsg :: User -> IO ()
 recvMsg user = do
     bstr <- Bstr.hGetLine $ userHndl user
     t <- getCurrentTime
-    let msg = Msg t (username user) (userActv user) bstr
+    let msg = Msg t (userName user) (userActv user) bstr
     atomically $ writeTChan ((userRooms user) Map.! (userActv user)) msg
 
+-- Generates a new map of rooms with duped tchans
 dupRooms :: Map ByteString (TChan Msg) -> STM (Map ByteString (TChan Msg))
 dupRooms = Map.foldrWithKey foldfn (return Map.empty)
     where
@@ -114,6 +123,7 @@ dupRooms = Map.foldrWithKey foldfn (return Map.empty)
             val' <- dupTChan val
             return $ Map.insert key val' acc'
 
+-- User initialization that passes off the user to the user handler
 newUser :: Handle -> MVar (Map ByteString (TChan Msg))-> IO ()
 newUser hand rooms' = do
     name <- getUsername hand
@@ -121,8 +131,13 @@ newUser hand rooms' = do
     --room <- getUserRooms hand
     usrrooms <- atomically $ dupRooms rooms
     let user = User name hand "" usrrooms
-    --announce user
-    userHandler user
+    bracket (announce user " has joined") (killUser) (userHandler)
+
+killUser :: User -> IO ()
+killUser user = do
+    flip announce " has left" user
+    msg <- systemMsg (Bstr.concat ["Goodbye ", userName user]) ""
+    hPutBuilder (userHndl user) $ msgToBuilder $ msg
 
 -- prompts user for name
 getUsername :: Handle -> IO ByteString
@@ -131,19 +146,23 @@ getUsername hand = do
     name <- Bstr.hGetLine hand
     return name
 
+-- asks the user what rooms they would like to be a part of
 getUserRooms :: Handle -> IO (Set ByteString)
 getUserRooms hand = return $ Set.singleton ("" :: ByteString)
 
+-- accepts a user and forks them into their own thread
 acceptAndFork :: Socket -> MVar (Map ByteString (TChan Msg)) -> IO ThreadId
 acceptAndFork sock rooms = do
     (hand, _, _) <- accept sock
-    forkIO $ newUser hand rooms
+    forkIO $ bracket (return hand) (hClose) (flip newUser rooms)
 
+-- adds a new room by name to a map of all available rooms
 addRoom :: ByteString -> Map ByteString (TChan Msg) -> STM (Map ByteString (TChan Msg))
 addRoom name map = do
     tc <- newBroadcastTChan
     return $ Map.insertWith (flip const) name tc map
 
+-- runs the chat server on a given Port. Initializes a default room
 mainApp :: PortID -> IO ()
 mainApp port = do
     startingRooms <- (atomically $ addRoom "" Map.empty)
